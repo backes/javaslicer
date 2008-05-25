9a001de9fb6cd3cefab47faad71eb268944dd6a3
hammacher <hammacher@ef2ef919-bb9b-4b18-8ff0-e5cc2bf85462>
2008-05-25 23:47:27 +0000

    git-svn-id: https://svn.st.cs.uni-sb.de/svn/st/projects/java-slicer/trunk@2784 ef2ef919-bb9b-4b18-8ff0-e5cc2bf85462

--- java-slicer-tracer/src/main/java/de/unisb/cs/st/javaslicer/tracer/classRepresentation/ArrayInstruction.java
+++ java-slicer-tracer/src/main/java/de/unisb/cs/st/javaslicer/tracer/classRepresentation/ArrayInstruction.java
@@ -1,14 +1,58 @@
 package de.unisb.cs.st.javaslicer.tracer.classRepresentation;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import de.unisb.cs.st.javaslicer.tracer.traceResult.ConstantIntegerTraceSequence;
+import de.unisb.cs.st.javaslicer.tracer.traceResult.ConstantLongTraceSequence;
+import de.unisb.cs.st.javaslicer.tracer.traceResult.ConstantTraceSequence;
 import de.unisb.cs.st.javaslicer.tracer.traceSequences.IntegerTraceSequence;
 import de.unisb.cs.st.javaslicer.tracer.traceSequences.ObjectTraceSequence;
 
 public class ArrayInstruction extends Instruction {
 
+    // when tracing, these two fields are set
+    private final ObjectTraceSequence arrayTrace;
+    private final IntegerTraceSequence indexTrace;
+
+    // after reloading the trace, these two fields are set
+    private final ConstantLongTraceSequence constArrayTrace;
+    private final ConstantIntegerTraceSequence constIndexTrace;
+
+
     public ArrayInstruction(final ReadMethod readMethod, final int lineNumber, final int opcode,
             final ObjectTraceSequence arrayTrace, final IntegerTraceSequence indexTrace) {
         super(readMethod, opcode, lineNumber);
-        // TODO Auto-generated constructor stub
+        this.arrayTrace = arrayTrace;
+        this.indexTrace = indexTrace;
+        this.constArrayTrace = null;
+        this.constIndexTrace = null;
+    }
+
+    public ArrayInstruction(final ReadMethod readMethod, final int lineNumber, final int opcode, final int index,
+            final ConstantLongTraceSequence constArrayTrace, final ConstantIntegerTraceSequence constIndexTrace) {
+        super(readMethod, opcode, lineNumber, index);
+        this.constArrayTrace = constArrayTrace;
+        this.constIndexTrace = constIndexTrace;
+        this.arrayTrace = null;
+        this.indexTrace = null;
+    }
+
+    @Override
+    public void writeOut(final ObjectOutputStream out) throws IOException {
+        assert this.arrayTrace != null && this.indexTrace != null;
+        assert this.constArrayTrace == null && this.constIndexTrace == null;
+
+        super.writeOut(out);
+        this.arrayTrace.writeOut(out);
+        this.indexTrace.writeOut(out);
+    }
+
+    public static ArrayInstruction readFrom(final ObjectInputStream in, final ReadMethod readMethod, final int opcode, final int index, final int lineNumber) throws IOException {
+        final ConstantLongTraceSequence constArrayTrace = (ConstantLongTraceSequence) ConstantTraceSequence.readFrom(in);
+        final ConstantIntegerTraceSequence constIndexTrace = (ConstantIntegerTraceSequence) ConstantTraceSequence.readFrom(in);
+        return new ArrayInstruction(readMethod, lineNumber, opcode, index, constArrayTrace, constIndexTrace);
     }
 
 }
--- java-slicer-tracer/src/main/java/de/unisb/cs/st/javaslicer/tracer/classRepresentation/Instruction.java
+++ java-slicer-tracer/src/main/java/de/unisb/cs/st/javaslicer/tracer/classRepresentation/Instruction.java
@@ -1,10 +1,22 @@
 package de.unisb.cs.st.javaslicer.tracer.classRepresentation;
 
-
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public abstract class Instruction {
 
     private static int nextIndex = 0;
+
+    private static final Class<?>[] instructions =
+        new Class<?>[] {
+            ArrayInstruction.class, FieldInstruction.class, IIncInstruction.class,
+            IntPush.class, JumpInstruction.class, LabelMarker.class,
+            LdcInstruction.class, MethodInvocationInstruction.class,
+            NewArrayInstruction.class, SimpleInstruction.class
+        };
 
     private final int index;
     protected final ReadMethod method;
@@ -12,12 +24,16 @@ public abstract class Instruction {
     private final int lineNumber;
 
     public Instruction(final ReadMethod readMethod, final int opcode, final int lineNumber) {
-        this.index = nextIndex++;
+        this(readMethod, opcode, lineNumber, nextIndex++);
         if (nextIndex < 0)
             throw new RuntimeException("Integer overflow in instruction index");
+    }
+
+    protected Instruction(final ReadMethod readMethod, final int opcode, final int lineNumber, final int index) {
         this.method = readMethod;
         this.opcode = opcode;
         this.lineNumber = lineNumber;
+        this.index = index;
     }
 
     public int getIndex() {
@@ -42,6 +58,54 @@ public abstract class Instruction {
 
     public int getBackwardInstructionIndex() {
         return this.index - 1;
+    }
+
+    public void writeOut(final ObjectOutputStream out) throws IOException {
+        // write out type of the instruction
+        boolean typeFound = false;
+        for (int i = 0; i < instructions.length; ++i) {
+            if (instructions[i].equals(getClass())) {
+                typeFound = true;
+                out.writeByte(i);
+                break;
+            }
+        }
+        if (!typeFound)
+            throw new RuntimeException("Class " + getClass() + " has no index to export");
+        out.writeInt(this.index);
+        out.writeInt(this.lineNumber);
+        out.writeInt(this.opcode);
+    }
+
+    public static Instruction readFrom(final ObjectInputStream in, final ReadMethod readMethod) throws IOException {
+        // first determine the type
+        final byte type = in.readByte();
+        if (type < 0 || type >= instructions.length)
+            throw new RuntimeException("Illegal instruction class index: " + type);
+        final int index = in.readInt();
+        final int lineNumber = in.readInt();
+        final int opcode = in.readInt();
+
+        final Class<?> instrClass = instructions[type];
+        try {
+            final Method readFromMethod = instrClass.getMethod("readFrom",
+                    ObjectInputStream.class, ReadMethod.class, int.class, int.class, int.class);
+            final Object o = readFromMethod.invoke(null, in, readMethod, opcode, index, lineNumber);
+            if (o instanceof Instruction)
+                return (Instruction)o;
+            throw new RuntimeException("readFrom does not return Instruction");
+        } catch (final SecurityException e) {
+            throw new RuntimeException(e);
+        } catch (final NoSuchMethodException e) {
+            throw new RuntimeException("Internal error: class " + instrClass + " does not implement readFrom", e);
+        } catch (final IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (final IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (final InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
