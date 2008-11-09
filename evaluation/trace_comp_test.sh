847320a2ef42c57e4233323e46ba44031466777a
hammacher <hammacher@ef2ef919-bb9b-4b18-8ff0-e5cc2bf85462>
2008-11-09 21:16:30 +0000

    git-svn-id: https://svn.st.cs.uni-sb.de/svn/st/projects/java-slicer/trunk@6496 ef2ef919-bb9b-4b18-8ff0-e5cc2bf85462

--- evaluation/trace_comp_test.sh
+++ evaluation/trace_comp_test.sh
@@ -20,35 +20,52 @@ size=small
 
 rm trace.log
 
+resultfilenr=0
+while [[ -e "result_$resultfilenr" ]]; do
+  resultfilenr=$((resultfilenr+1))
+done
+resultfile="result_"$resultfilenr
 
-# lusearch does not work
+printf '"run","testcase","normal msec","nocomp msec","nocomp MB","gzip msec","gzip MB","seq msec","seq MB"\n' >>$resultfile
 
-for testcase in antlr bloat chart eclipse fop hsqldb jython luindex lusearch pmd xalan; do
-  echo doing test $testcase
 
-  starttime=`date +'%s*1000+%N/1000000' | sed 's/+0*/+/'`
-  
-  time $SUN_DIR/bin/java -Xmx$MEMORY -jar dacapo-2006-10-MR2.jar -n $runs -s $size $testcase
-  
-  endtime=`date +'%s*1000+%N/1000000' | sed 's/+0*/+/'`
-  millis=$(( ($endtime) - ($starttime) ))
-
-  echo "##result: $testcase no_tracing: $millis msec"
-
-  for compression in none gzip sequitur; do
+for run in 1 2 3 4 5; do
+  for testcase in antlr bloat chart eclipse fop hsqldb jython luindex lusearch pmd xalan; do
+    echo doing test $testcase
+    printf '%d,"%s"' $run $testcase >>$resultfile
   
     starttime=`date +'%s*1000+%N/1000000' | sed 's/+0*/+/'`
-
-    time $SUN_DIR/bin/java -Xmx$MEMORY -javaagent:tracer.jar=logfile:trace.log,compression:$compression -jar dacapo-2006-10-MR2.jar -n $runs -s $size $testcase
-
+    
+    time $SUN_DIR/bin/java -Xmx$MEMORY -jar dacapo-2006-10-MR2.jar -n $runs -s $size $testcase
+    
     endtime=`date +'%s*1000+%N/1000000' | sed 's/+0*/+/'`
     millis=$(( ($endtime) - ($starttime) ))
-
-    sleep 3
-    fsize=`stat -c "scale=1; %b*%B/1024/1024" trace.log | bc`
-    rm trace.log
   
-    echo "##result: $testcase $compression: $millis msec, $fsize MB"
+    echo "##result: $testcase no_tracing: $millis msec"
+    printf ',%s' $millis >>$resultfile
+  
+    for compression in none gzip sequitur; do
+    
+      starttime=`date +'%s*1000+%N/1000000' | sed 's/+0*/+/'`
+  
+      time $SUN_DIR/bin/java -Xmx$MEMORY -javaagent:tracer.jar=logfile:trace.log,compression:$compression -jar dacapo-2006-10-MR2.jar -n $runs -s $size $testcase
+  
+      endtime=`date +'%s*1000+%N/1000000' | sed 's/+0*/+/'`
+      millis=$(( ($endtime) - ($starttime) ))
+  
+      sleep 3
+      fsize=`stat -c "scale=1; %b*%B/1024/1024" trace.log | bc`
+      rm trace.log
+    
+      echo "##result: $testcase $compression: $millis msec, $fsize MB"
+      printf ',%s,%s' $millis $fsize >>$resultfile
+    done
+    printf "\n" >>$resultfile
   done
 done
+
+echo
+echo
+echo "Results (also stored in $resultfile):"
+cat $resultfile
 
