c184ba5f79a638426745ddad0f82062a3eaa0a4c
hammacher <hammacher@ef2ef919-bb9b-4b18-8ff0-e5cc2bf85462>
2008-11-10 16:01:28 +0000

    git-svn-id: https://svn.st.cs.uni-sb.de/svn/st/projects/java-slicer/trunk@6508 ef2ef919-bb9b-4b18-8ff0-e5cc2bf85462

--- java-slicer-core/src/test/java/de/unisb/cs/st/javaslicer/ExceptionsTest1.java
+++ java-slicer-core/src/test/java/de/unisb/cs/st/javaslicer/ExceptionsTest1.java
@@ -16,7 +16,6 @@ public class ExceptionsTest1 extends AbstractSlicingTest {
         final List<Instruction> slice = getSlice("/traces/exceptions1", "main", "de.unisb.cs.st.javaslicer.tracedCode.Exceptions1.main:16:{c}");
         checkSlice(slice, new String[] {
                 "de.unisb.cs.st.javaslicer.tracedCode.Exceptions1.main:7 ACONST_NULL",
-                "de.unisb.cs.st.javaslicer.tracedCode.Exceptions1.main:7 ASTORE 2",
                 "de.unisb.cs.st.javaslicer.tracedCode.Exceptions1.main:11 ALOAD 2",
                 "de.unisb.cs.st.javaslicer.tracedCode.Exceptions1.main:11 INVOKESTATIC de/unisb/cs/st/javaslicer/tracedCode/Exceptions1.useArrays([I[I)I",
                 "de.unisb.cs.st.javaslicer.tracedCode.Exceptions1.main:13 ICONST_1",
