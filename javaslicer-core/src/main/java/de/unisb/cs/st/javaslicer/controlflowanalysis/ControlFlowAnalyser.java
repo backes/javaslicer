198588bd44b966bf067da6b15f550e2e305a5df8
hammacher <hammacher@ef2ef919-bb9b-4b18-8ff0-e5cc2bf85462>
2010-01-09 15:16:26 +0000

    sped up the critical method during slicing a lot
    git-svn-id: https://svn.st.cs.uni-sb.de/svn/st/projects/java-slicer/trunk@15252 ef2ef919-bb9b-4b18-8ff0-e5cc2bf85462

--- javaslicer-core/src/main/java/de/unisb/cs/st/javaslicer/controlflowanalysis/ControlFlowAnalyser.java
+++ javaslicer-core/src/main/java/de/unisb/cs/st/javaslicer/controlflowanalysis/ControlFlowAnalyser.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 
 import de.hammacher.util.UniqueQueue;
@@ -86,32 +87,65 @@ public class ControlFlowAnalyser {
 
     private void computeReachableNodes(ControlFlowGraph cfg) {
         UniqueQueue<InstrNode> queue = new UniqueQueue<InstrNode>(true);
+        InstrNode node;
+        // using this random to switch between FIFO and LIFO behaviour turned out
+        // to decrease the runtime a lot.
+        // on an example method with 706 nodes (the critical one in a big example run):
+        // - LIFO: 9.55 seconds
+        // - FIFO: 1.88 seconds
+        // - 0.5*LIFO + 0.5*FIFO: 0.23 seconds
+        Random rand = new Random();
+
+        // first, compute the reachable nodes:
         for (Instruction instr: cfg.getMethod().getInstructions())
             queue.add(cfg.getNode(instr));
+        while ((node = queue.poll()) != null) {
+            Iterator<InstrNode> succIt = node.getSuccessors().iterator();
+            if (succIt.hasNext()) {
+                boolean change = false;
+                ReachInstrNode reachNode = (ReachInstrNode) node;
+                Set<AbstractInstrNode> reachable = reachNode.getReachable();
+                while (succIt.hasNext()) {
+                    ReachInstrNode succ = (ReachInstrNode) succIt.next();
+                    change |= reachable.addAll(succ.getReachable());
+                }
+                if (change) {
+                    for (InstrNode pred : node.getPredecessors()) {
+                        if (rand.nextBoolean())
+                            queue.addLast(pred);
+                        else
+                            queue.addFirst(pred);
+                    }
+                }
+            }
+        }
 
-        InstrNode node;
+        // then, compute the surely reached nodes:
+        for (Instruction instr: cfg.getMethod().getInstructions())
+            queue.add(cfg.getNode(instr));
         while ((node = queue.poll()) != null) {
             Iterator<InstrNode> succIt = node.getSuccessors().iterator();
             if (succIt.hasNext()) {
                 boolean change = false;
                 ReachInstrNode reachNode = (ReachInstrNode) node;
                 ReachInstrNode succ1 = (ReachInstrNode) succIt.next();
-                Set<AbstractInstrNode> reachable = reachNode.getReachable();
-                change |= reachable.addAll(succ1.getReachable());
                 if (succIt.hasNext()) { // more than one successor?
                     Set<AbstractInstrNode> surelyReached = new HashSet<AbstractInstrNode>(succ1.getSurelyReached());
                     do {
                         ReachInstrNode succ2 = (ReachInstrNode) succIt.next();
                         surelyReached.retainAll(succ2.getSurelyReached());
-                        change |= reachable.addAll(succ2.getReachable());
                     } while (succIt.hasNext());
                     change |= reachNode.getSurelyReached().addAll(surelyReached);
                 } else {
                     change |= reachNode.getSurelyReached().addAll(succ1.getSurelyReached());
                 }
                 if (change) {
-                    for (InstrNode pred : node.getPredecessors())
-                        queue.addFirst(pred);
+                    for (InstrNode pred : node.getPredecessors()) {
+                        if (rand.nextBoolean())
+                            queue.addLast(pred);
+                        else
+                            queue.addFirst(pred);
+                    }
                 }
             }
         }
