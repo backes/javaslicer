c88948db338e4407b675a09ad64ea881fd14ce46
Clemens Hammacher <hammacher@cs.uni-saarland.de>
2013-05-24 15:12:52 +0200

    Switch license to GPLv3

--- javaslicer-tracer/src/main/java/de/unisb/cs/st/javaslicer/tracer/Tracer.java
+++ javaslicer-tracer/src/main/java/de/unisb/cs/st/javaslicer/tracer/Tracer.java
@@ -7,9 +7,18 @@
  * This file is part of the JavaSlicer tool, developed by Clemens Hammacher at Saarland University.
  * See http://www.st.cs.uni-saarland.de/javaslicer/ for more information.
  *
- * This work is licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/3.0/ or send a
- * letter to Creative Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
+ * JavaSlicer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JavaSlicer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with JavaSlicer. If not, see <http://www.gnu.org/licenses/>.
  */
 package de.unisb.cs.st.javaslicer.tracer;
 
